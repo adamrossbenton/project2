de907dfe781cb677f5afcbb1646354c76763de9b
Adam Ross Benton <adamrossbenton@gmail.com>
2021-10-06 16:50:49 -0700

    set up seed route

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ const restSchema = new Schema({
     restCuisine: String,
     restOpenHours: String,
     restHappsHours: String,
+    restHappsDeals: String,
     restCreator: String,
     restRating: Number,
 })
@@ -77,6 +78,22 @@ app.get("/", (req,res) => {
     res.send("Hello world")
 })
 
+// Seed
+app.get("/seed", (req,res) => {
+    const startRest = [
+        {restName: "Bangrak Market", restCity: "Seattle", restCuisine: "Thai", restOpenHours: "opentoclose", restHappsHours: "5 to 7", restHappsDeals: "lots", restCreator: "me", restRating: 1},
+        {restName: "Taqueria Cantina", restCity: "Seattle", restCuisine: "Mexican", restOpenHours: "open to close", restHappsHours: "3 to 6", restHappsDeals: "all kinds", restCreator: "also me", restRating: 10},
+        {restName: "Cyclops", restCity: "Seattle", restCuisine: "American", restOpenHours: "open to close", restHappsHours: "6 to 9", restHappsDeals: "a good amount", restCreator: "still me", restRating: 11},
+        {restName: "MOONTREE", restCity: "Seattle", restCuisine: "Sushi/Japanese", restOpenHours: "open to close", restHappsHours: "8 to close", restHappsDeals: "a handful", restCreator: "not me (jk lol)", restRating: 100},
+    ]
+
+    Rest.remove({}, (err,data) => {
+        Rest.create(startRest, (err,data) => {
+            res.json(data)
+        })
+    })
+})
+
 // Index
 app.get("/restaurants", (req,res) => {
     Rest.find({}, (err,rests) => {
@@ -95,6 +112,12 @@ app.get("/restaurants", (req,res) => {
 // Edit
 
 // Show
+app.get("/restaurants/:id", (req,res) => {
+    const id = req.params.id
+    // Rest.findById(id,(err,rest) => {
+    //     res.render("rest/show.ejs", {rest})
+    // })
+})
 
 ////////////////////////////////////////
 // LISTENER
