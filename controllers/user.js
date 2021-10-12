2161827b2fa8f0a517a59ce0650e1e91355329eb
Adam Ross Benton <adamrossbenton@gmail.com>
2021-10-11 20:09:51 -0700

    nvm it didnt work

--- controllers/user.js
+++ controllers/user.js
@@ -41,7 +41,7 @@ router.post("/login", (req,res) => {
             if (result) {
                 req.session.loggedIn = true
                 req.session.username = username
-                res.redirect("/restaurants")
+                res.redirect("/restaurants", {req})
             } else {
                 res.send("Incorrect password")
             }
