80b1c56d117ab87427a112c47f8c7b1ef76b9a2f
Adam Ross Benton <adamrossbenton@gmail.com>
2021-10-13 13:39:42 -0700

    nvm it broke it, will do it later

--- controllers/rest.js
+++ controllers/rest.js
@@ -66,16 +66,9 @@ router.post("/", (req,res) => {
 // Edit
 router.get("/:id/edit", (req,res) => {
     const id = req.params.id
-    if (req.session.loggedIn) {
-        Rest.findById(id, (err,rest) => {
-            res.render("rest/edit.ejs", {rest, loggedIn: req.session.loggedIn})
-        })
-    } else {
-        res.redirect("/user/login")
-    }
-    // Rest.findById(id, (err,rest) => {
-    //     res.render("rest/edit.ejs", {rest, loggedIn: req.session.loggedIn})
-    // })
+    Rest.findById(id, (err,rest) => {
+        res.render("rest/edit.ejs", {rest, loggedIn: req.session.loggedIn})
+    })
 })
 
 // Show
